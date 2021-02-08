e32aae67f2d0c8cb472bdb96b940b19807f1aa6d
joriszandbergen <62294353+joriszandbergen@users.noreply.github.com>
2021-02-08 13:35:38 +0100

    installing of all the necessary libraries

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+//import Global style document
+import GlobalStyles from "./components/GlobalStyles";
+//import Router
+import { Route } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyles />
+      <h1>EQUI web-app</h1>
     </div>
   );
 }
